9d88f795857d85f7676339ea3672a5b3aeec8007
PRG <VVMoskalchuk@mail.ru>
2018-04-14 21:35:40 +0500

    Json to Object

---
+++ server-ui/src/test/java/PointDTOTest.java
@@ -0,0 +1,25 @@
+
+import jdev.dto.PointDTO;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by jdev on 06.03.2017.
+ */
+public class PointDTOTest {
+
+    @Test
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        assertTrue(point.toJson().contains("\"lat\":56"));
+        System.out.println(point.toJson());
+
+        String jsonInString = point.toJson().toString();
+        Object obj = point.fromJson(jsonInString, Object.class);
+        System.out.println(obj);
+    }
+}
